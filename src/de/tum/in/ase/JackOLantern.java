3e188c9646deab15b803bd49dbae61a5a0c31e2d
Yunqing Wang <yunqing.wang@tum.de>
2022-10-06 20:12:14 +0200

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/JackOLantern.java
@@ -0,0 +1,22 @@
+package de.tum.in.ase;
+
+public class JackOLantern {
+    //TODO construct correct attributes
+
+
+    //TODO write constructor with parameters
+
+
+    public double getLanternWeight() {
+        //TODO calculate the total weight of your Jack O'Lantern
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+       /*
+       You can test your code here by constructing objects. Have fun :)
+       */
+    }
+}
